5a42cdfebcc948c43f60ae2189933b8c9f14f552
SankarKarthi <sankarkarthikeyan602.com>
2026-01-16 20:49:41 +0530

    google client id

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,6 +1,6 @@
 // Basic configuration for the frontend app
 const API_BASE_URL = "https://portfolio-buzz.onrender.com";
-const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "";
+const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "817927877456-aq8fupn0sdujqikouruk0p11bk4d9d61.apps.googleusercontent.com";
 
 export const config = {
     API_BASE_URL,
